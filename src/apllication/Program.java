a1fabbaf35bdcdb1891fcb56c0d213d5529edb6c
leandrobrotas <leandromacedo.tecnologia@gmail.com>
2020-11-12 07:24:01 -0300

    Department entity class

---
+++ src/apllication/Program.java
@@ -0,0 +1,13 @@
+package apllication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department department = new Department(1, "Books");
+		System.out.println(department);
+	}
+
+}
